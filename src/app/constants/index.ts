7de649c7d96f55b8a6919899660338485a91f61b
Brice Vignal <vignal.brice@gmail.com>
2025-09-14 02:52:41 +0200

    ZS/fix: update constants

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -2,11 +2,11 @@ export const EMAIL_ADDRESS =
   process.env.NEXT_PUBLIC_EMAIL_ADDRESS || "contact@zepices-soleil.fr";
 
 export const PHONE_NUMBER =
-  process.env.NEXT_PUBLIC_PHONE_NUMBER || "+33 6 12 34 56 78";
+  process.env.NEXT_PUBLIC_PHONE_NUMBER || "+33 6 16 24 77 81";
 
 export const WHATSAPP_LINK =
   process.env.NEXT_PUBLIC_WHATSAPP_LINK ||
-  "https://wa.me/33612345678";
+  "https://wa.me/330616247781";
 
 export const FADE_UP_ANIMATION = {
   initial: { opacity: 0, y: 28 },
