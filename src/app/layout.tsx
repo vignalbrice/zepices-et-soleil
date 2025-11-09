26a515de6140d43c5ae34b76869dda29e1bfcd0f
Brice Vignal <vignal.brice@gmail.com>
2025-11-09 16:58:51 +0100

    ZS/fix: add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -10,6 +11,12 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/icons.ico",
+  },
+};
 
 export default function RootLayout({
   children,
